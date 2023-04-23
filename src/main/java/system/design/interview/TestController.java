0e3968069561e93b554991753dfcc700493fd87b
minion.mo <minion.mo@kakaocorp.com>
2023-04-23 13:26:39 +0900

    docker spring boot app 추가

---
+++ src/main/java/system/design/interview/TestController.java
@@ -0,0 +1,16 @@
+package system.design.interview;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        log.info("하위");
+        return "test";
+    }
+}
