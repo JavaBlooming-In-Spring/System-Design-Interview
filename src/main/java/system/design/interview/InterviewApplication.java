849c0b7cd290ff77429fcc271220aad5d5969b5d
minion.mo <minion.mo@kakaocorp.com>
2023-04-23 12:34:33 +0900

    init project

---
+++ src/main/java/system/design/interview/InterviewApplication.java
@@ -0,0 +1,13 @@
+package system.design.interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/system/design/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package system.design.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
