1164c5b8efa2f6080009046c7a295af7d69f0ec8
woo-chang <clllickme@naver.com>
2023-05-14 04:36:09 +0900

    feat: 생성 시간을 위한 설정

---
+++ src/main/java/system/design/interview/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package system.design.interview.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
