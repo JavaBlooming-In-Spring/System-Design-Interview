585adac8e5453e97ac8a3120497e5f2568d51d0f
hyun98 <hyun0404woo@gmail.com>
2023-05-26 23:37:01 +0900

    fix

--- src/main/java/system/design/interview/TestController.java
+++ src/main/java/system/design/interview/TestController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 public class TestController {
-    private int cnt = 0;
     @GetMapping("/test")
     public String test() {
-        cnt++;
-//        System.out.println("" + cnt);
         return "test";
     }
 }
