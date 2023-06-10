e39412e915413454437cfcac9d75040e76ef7bb9
jojaeng2 <ds4ouj@naver.com>
2023-06-10 13:50:49 +0900

    refactor: url service name으로 변경

--- src/main/java/system/design/interview/TestController.java
+++ src/main/java/system/design/interview/TestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
+        log.info("Hello!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return "test";
     }
 }
